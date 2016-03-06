dc11efac7b85d2d1ca76c26c082f7276f9e00735
Sanu Sathyaseelan <sanu.sathyaseelan@itaas.dimensiondata.com>
2016-03-06 18:13:28 +1100

    [SS] Added empty test

---
+++ test/test-app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+
+describe('topshelf:app', function(){
+    describe('default', function(){
+       before(function(){
+           
+       }); 
+       
+       it('creates files', function(){
+           
+       });
+       
+       it('adds default service name', function(){
+           
+       });
+    });
+});
